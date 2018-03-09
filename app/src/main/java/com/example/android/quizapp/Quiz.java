0727120f20b28047dff4151d151a77ee96d63eee
Dorian <dgrove79@gmail.com>
2018-03-08 19:53:44 -0500

    Updated

--- app/src/main/java/com/example/android/quizapp/Quiz.java
+++ app/src/main/java/com/example/android/quizapp/Quiz.java
@@ -1,11 +1,12 @@
 package com.example.android.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class Quiz extends AppCompatActivity {
@@ -14,21 +15,28 @@ public class Quiz extends AppCompatActivity {
      * Variable to hold the amount of correct answers.
      */
     int correct = 0;
+    private boolean isSubmitted;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_quiz);
+        //Boolean Variable to distinguish between true (submitted) and false (not submitted).
+        boolean isSubmitted = false;
+
+
     }
+
 
     /**
      * Method to record correct answer for question 1.
      */
     public void questionOne() {
 
-        RadioButton rbOne = findViewById(R.id.question_1_correct);
+        RadioButton rbThree = findViewById (R.id.question_1_correct);
 
-        if (rbOne.isChecked()) {
+        if (rbThree.isChecked ()) {
             correct++;
         }
     }
@@ -38,9 +46,9 @@ public class Quiz extends AppCompatActivity {
      */
     public void questionTwo() {
 
-        RadioButton rbTwo = findViewById(R.id.question_2_correct);
+        RadioButton rbTwo = findViewById (R.id.question_2_correct);
 
-        if (rbTwo.isChecked()) {
+        if (rbTwo.isChecked ()) {
             correct++;
         }
     }
@@ -50,28 +58,33 @@ public class Quiz extends AppCompatActivity {
      */
     public void questionThree() {
 
-        CheckBox cb1 = findViewById(R.id.Kareem_Abdul_Jabbar);
-        CheckBox cb2 = findViewById(R.id.Wilt_Chamberlin);
-        CheckBox cb3 = findViewById(R.id.Kobe_Bryant);
-        CheckBox cb4 = findViewById(R.id.Michael_Jordan);
-        CheckBox cb5 = findViewById(R.id.Karl_Malone);
+        CheckBox cb1 = findViewById (R.id.cb1);
+        CheckBox cb2 = findViewById (R.id.cb2);
+        CheckBox cb3 = findViewById (R.id.cb3);
+        CheckBox cb4 = findViewById (R.id.cb4);
+        CheckBox cb5 = findViewById (R.id.cb5);
 
-
-        if (!cb1.isChecked() && cb2.isChecked() && cb3.isChecked() && cb4.isChecked() && !cb5.isChecked())
-
-        if (cb1.isChecked() && cb5.isChecked()) {
-            correct++;
+        if (cb1.isChecked ()) {
+            if (!cb2.isChecked ()) {
+                if (!cb3.isChecked ()) {
+                    if (!cb4.isChecked ()) {
+                        if (cb5.isChecked ())
+                            correct++;
+                    }
+                }
+            }
         }
     }
+
 
     /**
      * Method to record correct answer for question 4.
      */
     public void questionFour() {
 
-        RadioButton rbFour = findViewById(R.id.question_4_correct);
+        RadioButton rbTwo = findViewById (R.id.question_4_correct);
 
-        if (rbFour.isChecked()) {
+        if (rbTwo.isChecked ()) {
             correct++;
         }
     }
@@ -82,9 +95,9 @@ public class Quiz extends AppCompatActivity {
      */
     public void questionFive() {
 
-        RadioButton rbFiveTwo = findViewById(R.id.question_5_correct);
+        RadioButton rbTwo = findViewById (R.id.question_5_correct);
 
-        if (rbFiveTwo.isChecked()) {
+        if (rbTwo.isChecked ()) {
             correct++;
         }
     }
@@ -93,27 +106,47 @@ public class Quiz extends AppCompatActivity {
      * Compile answers.
      */
 
-    private void calculateAnswers(){
-        questionOne();
-        questionTwo();
-        questionThree();
-        questionFour();
-        questionFive();
+    private void calculateAnswers() {
+
     }
 
     /**
      * Method to display question results in a toast message.
      */
     public void submitAnswers(View view) {
-
         calculateAnswers ();
-
-        EditText userName = findViewById(R.id.name_text_input);
-        String name = userName.getText().toString();
-
+        //Check for isSubmitted value
+        if (isSubmitted) {
+            Toast.makeText (this, "You've already submitted your answers", Toast.LENGTH_SHORT).show ();
+        } else {
+            calculateAnswers ();
+            questionOne ();
+            questionTwo ();
+            questionThree ();
+            questionFour ();
+            questionFive ();
+            isSubmitted = true;
+        }
+        EditText userName = findViewById (R.id.name_text_input);
+        String name = userName.getText ().toString ();
         String resultsMessage = name + ", You got " + correct + " out of 5 answers correct.";
-        Toast.makeText(this, resultsMessage, Toast.LENGTH_LONG).show();
+        Toast.makeText (this, resultsMessage, Toast.LENGTH_LONG).show ();
+    }
 
+    /**
+     * When clicked this method will reset the radio buttons in quiz to none selected
+     */
+    public void Reset(View view) {
+        correct = 0;
+
+        RadioGroup rg_q1 = findViewById (R.id.rg_q1);
+        rg_q1.clearCheck ();
+        RadioGroup rg_q2 = findViewById (R.id.rg_q2);
+        rg_q2.clearCheck ();
+        RadioGroup rg_q3 = findViewById (R.id.rg_q3);
+        rg_q3.clearCheck ();
+        RadioGroup rg_q4 = findViewById (R.id.rg_q4);
+        rg_q4.clearCheck ();
     }
 
 }
